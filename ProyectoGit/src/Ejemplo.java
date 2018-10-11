cfb5e6069182741243e3c41fc3e7d040dca877e8
SUITE <SUITE@SUITEE102-ST21.cibertec.edu.pe>
2018-10-10 20:55:45 -0500

    sadasd

---
+++ ProyectoGit/src/Ejemplo.java
@@ -0,0 +1,31 @@
+
+public class Ejemplo {
+	
+	public static void main(String[] args) {
+		System.out.println("Git Hub");	
+		
+		System.out.println("ella no me ama");
+		
+		int numero1 =1;
+		int numero2 =1;
+		int resultado;
+		
+		resultado = numero1  + numero2;
+		
+		System.out.println(resultado);
+		
+				
+		
+	}
+	
+	
+	public void metodo2(){
+		
+	}
+	public void metodo3(){
+		
+	}
+	public void metodo4(){
+		
+	}
+}
